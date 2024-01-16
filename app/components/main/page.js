0a797de12c427189844c6496b4c6e9ee9009ef41
Reyone Chaudhary <csit22081046_reyone@achsnepal.edu.np>
2024-01-16 21:18:02 +0545

    Homepage and navbar created

--- app/components/main/page.js
+++ app/components/main/page.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Image from "next/image";
 
 const Main = () => {
-  return <div>Reyone</div>;
+  return (
+    <div>
+      <div className="bg-[url('/home.png')] w-screen h-screen flex object-fill "></div>
+    </div>
+  );
 };
 
 export default Main;
---
+++ app/components/navbar/page.js
@@ -0,0 +1,29 @@
+"use client";
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { FaSearch } from "react-icons/fa";
+
+const Navbar = () => {
+  return (
+    <div className="bg-[#1E1E1E] px-5 flex items-center justify-between">
+      <div className="">
+        <Image alt="logo" src="/logo.png" width={100} height={100} />
+      </div>
+      <div className="items-center flex justify-around w-1/2">
+        <div className=" flex justify-evenly w-2/3 ">
+          <Link href="">HOME</Link>
+          <Link href="">MENU</Link>
+          <Link href="">ABOUT US</Link>
+          <Link href="">CONTACT US</Link>
+        </div>
+        <div className="items-center w-1/4 flex justify-around">
+          <FaSearch />
+          <button className="bg-[#FF9E0B] px-4 py-1 ">ENQUIRY</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
