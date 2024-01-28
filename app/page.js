ff82d5b47fa01c243a4fad8f1d1e6cd68a75e89d
Bipasna Dulal <bipasna.dulal75@gmail.com>
2024-01-28 19:53:28 +0545

    Deleted carousel component

--- app/page.js
+++ app/page.js
@@ -1,13 +1,12 @@
 import Image from "next/image";
 import Main from "./components/main/page";
 import Menu from "./components/menu/page";
-import Elasticcarousel from "./components/carousel/Elasticcarousel";
+
 
 export default function Home() {
   return (
     <>
       <Main />
-      <Elasticcarousel/>
       <Menu/>
     </>
   );
