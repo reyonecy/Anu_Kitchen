bd4276712b25816ab082d32c6325c33ddaad94c9
Reyone Chaudhary <csit22081046_reyone@achsnepal.edu.np>
2024-01-28 10:47:01 +0545

    Footer works

--- app/components/menu/page.js
+++ app/components/menu/page.js
@@ -1,24 +1,25 @@
 import React from "react";
 import Image from "next/image";
 
-const Menu=()=>{
-    return(
-        <div className="bg-[#1E1E1E]">
-            <p className="text-center text-4xl mt-5 tracking-wider font-montserrat">MENU</p>
-            <div className="flex mt-7">
-                <div>
-                    <img src="/images/Menu.jpg" alt="menu1"/>
-                </div>
-                <div>
-                    <img src="/images/Menu2.jpg" alt="menu2"/>
-                </div>
+const Menu = () => {
+  return (
+    <div className="bg-[#1E1E1E]">
+      <p className="text-center text-4xl mt-5 tracking-wider font-montserrat">
+        MENU
+      </p>
+      <div className="flex mt-7">
+        <div>
+          <img src="/images/Menu.jpg" alt="menu1" />
         </div>
-        <div className="w-30">
-            <img src="/images/Menu3.jpg" alt="menu3"/>
+        <div>
+          <img src="/images/Menu2.jpg" alt="menu2" />
         </div>
-        </div>
-        
-    );
-}
+      </div>
+      <div className="w-30">
+        <img src="/images/Menu3.jpg" alt="menu3" />
+      </div>
+    </div>
+  );
+};
 
 export default Menu;
